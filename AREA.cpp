d7a128064c950fa2c8b1f0789e685ca43a8a2b80
Kashaf Fatima <kashaffatima2006@gmail.com>
2024-10-09 08:02:22 -0700

    Add files via upload
    BASIC AREA FORMUJLA IN C++.

---
+++ AREA.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+  /* int r;
+   float area;
+   const float  PI =3.142;
+   cout<<"Enter the radius :";
+   cin>>r;
+   area=2*PI*r*r;
+   cout<<"The area is :"<<area;
+   */
+   
+   bool size= 1;
+   cout<<!(size);
+	return 0;
+}
